54a8c87be62e4715ec0cfefe8814d63acf2b3d58
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-09-26 07:24:31 -0400

    #1 Eslint & Babel ES6

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-default-export': 'off',
+  },
+};
