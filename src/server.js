29a0148f7052bca77c64571a2bcd8b31baeb7ec2
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-09-27 07:19:37 -0400

    #4 adjusts for deploy

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ Mongoose.connect(process.env.DATABASE_CONNECTION, {
   useCreateIndex: true,
 });
 
-App.get('/', (_, res) => res.send({ content: Mongoose.version }));
+App.get('/', (_, res) => res.send({ message: 'API Ideas' }));
 
 const port = process.env.PORT || 3334;
 
 App.listen(port);
+
+console.log(`API WORKS ON ${process.env.NODE_ENV}:${port}`);
