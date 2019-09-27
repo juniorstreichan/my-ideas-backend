3bf3215cc1eb711f2ebc91233f65570a19da5bf7
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-09-26 21:27:02 -0400

    #3 Service layer ok

---
+++ __tests__/unit/service/ProjectService.test.js
@@ -0,0 +1,60 @@
+import faker from 'faker';
+import ProjectService from '../../../src/modules/project/service/ProjectService';
+import { connectMongoDBTest, disconnectMongoDBTest } from '../../tools/dbUtils';
+import { generateProject } from '../../tools/mockData';
+
+describe('Test suite for ProjectService', () => {
+  beforeAll(async () => {
+    await connectMongoDBTest();
+  });
+  afterAll(async () => {
+    await disconnectMongoDBTest();
+  });
+  let project = null;
+
+  it('should insert a Project', async () => {
+    project = await ProjectService.insert(generateProject());
+    expect(project).not.toBeNull();
+    expect(project).not.toBeUndefined();
+  });
+
+  it('should find Project by name', async () => {
+    const searchProject = await ProjectService.findByName(project.name);
+
+    expect(searchProject.ideas.lenght).toBe(project.ideas.lenght);
+    expect(searchProject.name).toBe(project.name);
+    expect(searchProject).not.toBeNull();
+    expect(searchProject).not.toBeUndefined();
+  });
+
+  it('should find Project by id', async () => {
+    const searchProject = await ProjectService.findById(project._id);
+
+    expect(searchProject.ideas.length).toBe(project.ideas.length);
+    expect(searchProject.name).toBe(project.name);
+    expect(searchProject).not.toBeNull();
+    expect(searchProject).not.toBeUndefined();
+  });
+
+  it('should add a idea in Project', async () => {
+    await ProjectService.addIdea(project._id, { title: faker.name.title(), description: faker.lorem.sentence(1) });
+
+    const compareProject = await ProjectService.findById(project._id);
+
+    expect(compareProject.name).toBe(project.name);
+    expect(compareProject).not.toBeNull();
+    expect(compareProject).not.toBeUndefined();
+    expect(compareProject.ideas.length).toBe(project.ideas.length + 1);
+  });
+
+  it('should remove a idea in Project', async () => {
+    await ProjectService.removeIdea(project._id, project.ideas[0]._id);
+
+    const compareProject = await ProjectService.findById(project._id);
+
+    expect(compareProject.name).toBe(project.name);
+    expect(compareProject).not.toBeNull();
+    expect(compareProject).not.toBeUndefined();
+    expect(compareProject.ideas.length).toBe(project.ideas.length);
+  });
+});
--- src/modules/project/service/ProjectService.js
+++ src/modules/project/service/ProjectService.js
@@ -1,12 +1,51 @@
+import Project from '../repository/Project';
 
 class ProjectService {
-  async insert(project) { }
+  async insert(project) {
+    if (project && typeof project === 'object') {
+      const newProject = await Project.create(project);
+      return newProject;
+    }
+    throw new Error('Project invalid!');
+  }
 
-  async findByName(name) { }
+  async findByName(name = '') {
+    if (name && name.trim !== '') {
+      const project = await Project.findOne({ name });
+      return project;
+    }
+    throw new Error('Invalid name!');
+  }
 
-  async addIdea(idProject, idea) { }
+  async findById(id = '') {
+    if (id) {
+      const project = await Project.findById(id);
+      return project;
+    }
+    throw new Error('Invalid id!');
+  }
 
-  async removeIdea(idProject, idea) { }
+  async addIdea(idProject = '', idea) {
+    if (idProject && idea) {
+      await Project.update(
+        { _id: idProject },
+        { $push: { ideas: idea } },
+      );
+      return;
+    }
+    throw new Error('Invalid id!');
+  }
+
+  async removeIdea(idProject, idIdea) {
+    if (idProject && idIdea) {
+      await Project.update(
+        { _id: idProject },
+        { $pull: { ideas: { _id: idIdea } } },
+      );
+      return;
+    }
+    throw new Error('Invalid id!');
+  }
 }
 
 export default new ProjectService();
